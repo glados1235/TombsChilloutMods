0315e3ca2445b85660177033a4038c2cbfecc39a
Glados1235 <glados12351235@gmail.com>
2022-07-28 14:23:43 -0700

    Add project files.

---
+++ PlayerRotateMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using MyProject;
+// ...
+[assembly: MelonInfo(typeof(playerRotateMod), "PlayerRotateMod", "1", "Tomb")]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PlayerRotateMod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PlayerRotateMod")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("819a8e0d-9797-4fb4-b6c8-576832c73fab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PlayerRotateMod/playerRotateMod.cs
@@ -0,0 +1,61 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace MyProject
+{
+
+
+    public class playerRotateMod : MelonMod
+    {
+
+
+        public static bool rotateToggle;
+        public static GameObject playerController;
+        GameObject childPlayer = playerController.transform.GetChild(0).gameObject;
+        public override void OnApplicationStart()
+        {
+
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            buildIndex = 1;
+            sceneName = "DontDestroyOnLoad";
+            playerController = GameObject.Find("_PLAYERLOCAL");
+
+        }
+
+        public override void OnUpdate()
+        {
+            if (rotateToggle)
+            {
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    childPlayer.transform.Rotate(1, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    childPlayer.transform.Rotate(-1, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    childPlayer.transform.Rotate(0, 0, 1);
+                }
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    childPlayer.transform.Rotate(0, 0, -1);
+                }
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                rotateToggle = !rotateToggle;
+                playerController.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+
+
+        }
+    }
+}
